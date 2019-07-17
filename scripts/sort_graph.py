8103c2b2866c8066ad27fc50a002bdcfee15bc18
ivanvikhrev <l4ind@mail.ru>
2019-07-18 00:02:39 +0300

    fixes

--- scripts/sort_graph.py
+++ scripts/sort_graph.py
@@ -4,10 +4,12 @@ from tensorflow.tools.graph_transforms import TransformGraph
 
 parser = argparse.ArgumentParser(description='Sort inference graph')
 parser.add_argument('--input', required=True, help='Path to frozen TensorFlow graph.')
+parser.add_argument('--output', required=True, help='Path to output.')
+args = parser.parse_args()
 
-with tf.gfile.FastGFile('frozen_inference_graph.pb', 'rb') as f:
+with tf.gfile.FastGFile(args.input, 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     graph_def = TransformGraph(graph_def, ['image_tensor'], ['detection_boxes', 'detection_classes', 'detection_scores', 'num_detections'], ['sort_by_execution_order'])
-    with tf.gfile.FastGFile('sorted_inference_graph.pb', 'wb') as f:
+    with tf.gfile.FastGFile(args.output, 'wb') as f:
         f.write(graph_def.SerializeToString())
